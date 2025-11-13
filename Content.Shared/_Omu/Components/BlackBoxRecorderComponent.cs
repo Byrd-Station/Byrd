2c4b2719af2f58b09c3e811b5cac0fa3149ddfdc
Fat Engineer <fatengineergaming2007@gmail.com>
2025-11-13 10:59:30 -0500

    adds the blackbox kind of

---
+++ Content.Shared/_Omu/Components/BlackBoxRecorderComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._Omu.Components;
+
+/// <summary>
+/// This component is being used for the blackbox recorder, not used for much right now.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class BlackBoxRecorderComponent : Component;
