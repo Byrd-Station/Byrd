4aef68f1c2d174d72637ab252a0c2dcb4fd1700b
Orion-416 <cliveocelot@gmail.com>
2025-11-22 15:22:56 +0000

    Fix asakim

---
+++ Content.Shared/_Omu/Components/TranslatorImplantBlockerComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+namespace Content.Shared._Omu.Components;
+
+/// <summary>
+/// Prevents the instalation of translator implants
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class TranslatorImplantBlockerComponent : Component;
