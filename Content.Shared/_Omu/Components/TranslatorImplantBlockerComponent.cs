3658c0a49049aa9c318b476881eea3970339a84c
RichardBlonski <48651647+RichardBlonski@users.noreply.github.com>
2025-11-23 14:57:19 +0200

    Minor spelling & spacing fixes on `TranslatorImplantBlockerComponet.cs`

--- Content.Shared/_Omu/Components/TranslatorImplantBlockerComponent.cs
+++ Content.Shared/_Omu/Components/TranslatorImplantBlockerComponent.cs
@@ -1,8 +1,9 @@
 using Robust.Shared.GameStates;
+
 namespace Content.Shared._Omu.Components;
 
 /// <summary>
-/// Prevents the instalation of translator implants
+/// Prevents the installation of translator implants
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class TranslatorImplantBlockerComponent : Component;
