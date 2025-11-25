d6792a5eaf9217a22bc47ef7b20d2beb7ed338a4
XWasHere <xwashere0@protonmail.com>
2025-11-25 12:07:40 -0500

    rework the rules smite

--- Content.Omu.Common/CCVar/CCVars.Omu.cs
+++ Content.Omu.Common/CCVar/CCVars.Omu.cs
@@ -6,7 +6,7 @@ namespace Content.Omu.Common.CCVar;
 public sealed partial class OmuCVars
 {
     /// <summary>
-    ///     How many seconds a player will have to wait to close the rules smite window.
+    ///     How many seconds a player will have to wait to close the rules smite popup.
     /// </summary>
     public static readonly CVarDef<float> RulesSmiteTime =
         CVarDef.Create("rulessmite.time", 60f, CVar.SERVER | CVar.REPLICATED);
--- Content.Omu.Server/Administration/Systems/OmuAdminVerbSystem.Smites.cs
+++ Content.Omu.Server/Administration/Systems/OmuAdminVerbSystem.Smites.cs
@@ -1,11 +1,15 @@
-using Content.Omu.Server.RulesSmite.UI;
+using Content.Omu.Common.CCVar;
 using Content.Server.Administration.Managers;
 using Content.Server.EUI;
 using Content.Shared.Administration;
+using Content.Shared.CCVar;
 using Content.Shared.Database;
+using Content.Shared.Info;
 using Content.Shared.Verbs;
 using Robust.Server.Player;
+using Robust.Shared.Configuration;
 using Robust.Shared.Map.Components;
+using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Utility;
 
@@ -16,12 +20,15 @@ public sealed partial class OmuAdminVerbSystem
     [Dependency] private readonly IAdminManager _admin = default!;
     [Dependency] private readonly EuiManager _eui = default!;
     [Dependency] private readonly IPlayerManager _player = default!;
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
 
     private void AddSmiteVerbs(GetVerbsEvent<Verb> args)
     {
         if (!SmitesAllowed(args))
             return;
 
+        // If someone has the FUN flag they probably have the ADMIN flag
         if (_player.TryGetSessionByEntity(args.Target, out var session))
         {
             var rulesName = Loc.GetString("admin-smite-rules-name").ToLowerInvariant();
@@ -32,7 +39,14 @@ public sealed partial class OmuAdminVerbSystem
                 Icon = new SpriteSpecifier.Rsi(new ("Mobs/Species/Human/organs.rsi"), "brain"),
                 Act = () =>
                 {
-                    _eui.OpenEui(new RulesSmiteEui(), session);
+                    var message = new SendRulesInformationMessage
+                    {
+                        PopupTime = _cfg.GetCVar(OmuCVars.RulesSmiteTime),
+                        CoreRules = _cfg.GetCVar(CCVars.RulesFile),
+                        ShouldShowRules = true,
+                        AllowBypass = false
+                    };
+                    _net.ServerSendMessage(message, session.Channel);
                 },
                 Impact = LogImpact.Extreme,
                 Message = string.Join(": ", rulesName, Loc.GetString("admin-smite-rules-description"))
