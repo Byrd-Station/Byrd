244d00fa95f17c827cc1cbb1b2e368e05b3252b9
RichardBlonski <48651647+RichardBlonski@users.noreply.github.com>
2025-11-26 16:14:45 +0200

    Moved Omu Code into its own `SocialAnxietySystem.Omu.cs`

--- Content.Goobstation.Shared/Traits/Systems/SocialAnxietySystem.cs
+++ Content.Goobstation.Shared/Traits/Systems/SocialAnxietySystem.cs
@@ -1,13 +1,14 @@
 using Content.Goobstation.Common.Traits.Components;
 using Content.Shared.Interaction.Events;
-using Content.Shared.IdentityManagement;
-using Content.Shared.InteractionVerbs;
-using Content.Shared.InteractionVerbs.Events;
 using Content.Shared.Standing;
 using Content.Shared.Popups;
 using Content.Shared.Stunnable;
+
+// Omu Station
 using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
+using Content.Shared.InteractionVerbs;
+using Content.Shared.InteractionVerbs.Events;
 
 namespace Content.Goobstation.Shared.Traits.Systems;
 
@@ -16,55 +17,33 @@ public sealed partial class SocialAnxietySystem : EntitySystem
     [Dependency] private readonly StandingStateSystem _standingSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
     [Dependency] private readonly SharedStunSystem _stunSystem = default!;
-    [Dependency] private readonly INetManager _net = default!;
 
+    // Omu Station
+    [Dependency] private readonly INetManager _net = default!;
     public ProtoId<InteractionVerbPrototype>[] PrototypeHug { get; } =
     [
         "Hug",
         "Pet",
     ];
+    // END
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<SocialAnxietyComponent, InteractionSuccessEvent>(OnHug);
+
+        // Omu
         SubscribeLocalEvent<InteractionVerbDoAfterEvent>(OnVerbHug);
     }
+
+    /*
+     // Omu Station - Replaced by SocialAnxietySystem.Omu.cs
     private void OnHug(EntityUid uid, SocialAnxietyComponent component, ref InteractionSuccessEvent args)
     {
         _standingSystem.Down(uid);
         _stunSystem.TryStun(uid, TimeSpan.FromSeconds(component.DownedTime), true);
         var mobName = Identity.Name(uid, EntityManager);
-        if(_net.IsServer)
-            _popupSystem.PopupEntity(Loc.GetString("social-anxiety-hugged", ("user", mobName)), uid, PopupType.MediumCaution);
+        _popupSystem.PopupEntity(Loc.GetString("social-anxiety-hugged", ("user", mobName)), uid, PopupType.MediumCaution);
     }
-
-    private void OnVerbHug(InteractionVerbDoAfterEvent args)
-    {
-        // we HAVE to subscribe to every InteractionVerbDoAfterEvent that fires.
-        // because I don't think absolute EE supercode allows us to do it normally.
-        if (!TryComp<SocialAnxietyComponent>(args.Target, out var component))
-            return;
-        if (!args.Target.HasValue)
-            return;
-
-        foreach (var prototype in PrototypeHug)
-        {
-            if (args.VerbPrototype != prototype)
-                continue;
-            var uid = args.Target.Value;
-            _standingSystem.Down(uid);
-            _stunSystem.TryStun(uid, TimeSpan.FromSeconds(component.DownedTime), true);
-            var mobName = Identity.Name(uid, EntityManager);
-            if(_net.IsServer)
-            {
-                _popupSystem.PopupEntity(Loc.GetString("social-anxiety-hugged", ("user", mobName)),
-                    uid,
-                    PopupType.MediumCaution);
-            }
-
-            break;
-        }
-
-    }
+    */
 }
