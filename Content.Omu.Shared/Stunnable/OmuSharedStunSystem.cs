6c9735de34ff5f3ae07564fe2341dde66a42089c
NotActuallyMarty <27968892+NotActuallyMarty@users.noreply.github.com>
2026-03-22 00:46:54 +0200

    Apply suggestion from @NotActuallyMarty

--- Content.Omu.Shared/Stunnable/OmuSharedStunSystem.cs
+++ Content.Omu.Shared/Stunnable/OmuSharedStunSystem.cs
@@ -3,30 +3,21 @@ using Content.Shared.Pulling.Events;
 
 namespace Content.Omu.Shared.Stunnable;
 
+// Todo: move this elsewhere with upstream grab refactor. maybe upstream this.
 public sealed class OmuSharedStunSystem : EntitySystem
 {
-    
     public override void Initialize()
     {
-        // Attempt event subscriptions.
         SubscribeLocalEvent<StunnedComponent, AttemptStopPullingEvent>(HandleStopPull);
     }
-
-    #region Attempt Event Handling
-    
     private void HandleStopPull(EntityUid uid, StunnedComponent _, ref AttemptStopPullingEvent args)
     {
         if (args.User == null || !Exists(args.User.Value))
             return;
-
         if (args.User.Value == uid)
         {
             //TODO: UX feedback. Simply blocking the normal interaction feels like an interface bug
-
             args.Cancelled = true;
         }
-
     }
-
-    #endregion
 }
