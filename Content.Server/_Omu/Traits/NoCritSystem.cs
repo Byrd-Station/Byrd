a084dc4e61918fac9d2033ae266c13ea6385ae7b
pinktanuki <me@gaywizard.xyz>
2026-03-31 22:17:09 -0400

    Added comments to NoCritSystem.cs

--- Content.Server/_Omu/Traits/NoCritSystem.cs
+++ Content.Server/_Omu/Traits/NoCritSystem.cs
@@ -19,11 +19,13 @@ public sealed class NoCritSystem : EntitySystem
         SubscribeLocalEvent<NoCritComponent, MobStateChangedEvent>(OnMobStateChanged);
     }
 
+    // Core of the NoCrit system, forces the entity to skip the Crit state
     private void OnStartup(EntityUid uid, NoCritComponent component, ComponentStartup args)
     {
         if (!TryComp<MobThresholdsComponent>(uid, out var thresholds))
             return;
 
+        // Sets the thresholds for each species to be appropriate for what they would be before Crit state
         var maxHp = 100;
         if (TryComp<HumanoidAppearanceComponent>(uid, out var humanoid) &&
             (humanoid.Species == "Avali" || humanoid.Species == "Resomi"))
@@ -35,6 +37,7 @@ public sealed class NoCritSystem : EntitySystem
         _thresholds.SetMobStateThreshold(uid, maxHp, MobState.Dead, thresholds);
     }
 
+    // Allows for the entity to deathgasp when entering into the death state instantly
     private void OnMobStateChanged(EntityUid uid, NoCritComponent component, MobStateChangedEvent args)
     {
         if (args.NewMobState == MobState.Dead)
