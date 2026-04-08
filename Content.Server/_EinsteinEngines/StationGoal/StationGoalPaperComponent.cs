726eb66595c03fde31db97ad7620b7385104a081
Orion-416 <cliveocelot@gmail.com>
2026-04-08 17:29:13 +0100

    station goals :3

---
+++ Content.Server/_EinsteinEngines/StationGoal/StationGoalPaperComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Server._EinsteinEngines.StationGoal
+{
+    /// <summary>
+    ///     Paper with a written station goal in it.
+    /// </summary>
+    [RegisterComponent]
+    public sealed partial class StationGoalPaperComponent : Component { }
+}
